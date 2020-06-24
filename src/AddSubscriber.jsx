4a4af8005caff6d66944ceaf5284b786d7280686
Kabir <kabir4691@gmail.com>
2020-06-24 08:17:34 +0530

    Added state in AddSubscriber and App components

--- src/AddSubscriber.jsx
+++ src/AddSubscriber.jsx
@@ -3,6 +3,20 @@ import Header from './Header'
 import './AddSubscriber.css'
 
 export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: 0,
+      name: '',
+      phone: ''
+    }
+  }
+
+  inputChangedHandler = (e) => {
+    this.state[e.target.name] = e.target.value;
+    this.setState(this.state);
+  }
+
   render() {
     return (
       <div>
@@ -12,18 +26,18 @@ export default class extends React.Component {
           <form className='subscriber-form'>
             <label htmlFor='name' className='label-control'>Name: </label>
             <br />
-            <input name='name' type='text' placeholder='Name' className='input-control'/>
+            <input name='name' type='text' placeholder='Name' className='input-control' onChange={this.inputChangedHandler}/>
             <br />
             <label htmlFor='phone' className='label-control'>Phone: </label>
             <br />
-            <input name='phone' type='text' placeholder='Phone' className='input-control'/>
+            <input name='phone' type='text' placeholder='Phone' className='input-control' onChange={this.inputChangedHandler}/>
             <br />
             <div className='subscriber-info-container'>
               <span className='subscriber-to-add-heading'>Subscriber to be added</span>
               <br />
-              <span className='subscriber-info'>Name: </span>
+    <span className='subscriber-info'>Name: {this.state.name}</span>
               <br />
-              <span className='subscriber-info'>Phone: </span>
+    <span className='subscriber-info'>Phone: {this.state.phone}</span>
             </div>
             <button className='button button-add'>Add</button>
           </form>
--- src/App.js
+++ src/App.js
@@ -2,40 +2,54 @@ import React from 'react';
 import Header from './Header';
 import './App.css'
 
-function App() {
-  const subscribers = [
-    { 
-      id: 1,
-      name: 'Kabir',
-      mobile: 9876543210
-    },
-    { 
-      id: 2,
-      name: 'Nazir',
-      mobile: 9012345678
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subscriberList: []
     }
-  ]
-  return (
-    <div>
-      <Header heading="PHONE DIRECTORY"/>
-      <div className="content-container">
-        <button className="button button-add">ADD</button>
-        <div className="heading-container">
-          <span className="heading-item">NAME</span>
-          <span className="heading-item">PHONE</span>
-        </div>
-        {subscribers.map(item => {
-          return (
-            <div className="row-container" key={item.id}>
-              <span className="row-item">{item.name}</span>
-              <span className="row-item">{item.mobile}</span>
-              <button className="button button-delete">DELETE</button>
-            </div>
-          )
-        })}
-      </div>
-    </div>
-  );
-}
+  }
 
-export default App;
+  render() {
+    return (
+      <div>
+        <Header heading="PHONE DIRECTORY"/>
+        <div className="content-container">
+          <button className="button button-add">ADD</button>
+          <div className="heading-container">
+            <span className="heading-item">NAME</span>
+            <span className="heading-item">PHONE</span>
+          </div>
+          {this.state.subscriberList.map(item => {
+            return (
+              <div className="row-container" key={item.id}>
+                <span className="row-item">{item.name}</span>
+                <span className="row-item">{item.mobile}</span>
+                <button className="button button-delete">DELETE</button>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.setState({
+      subscriberList: this.state.subscriberList.concat(
+        [
+          { 
+            id: 1,
+            name: 'Kabir',
+            mobile: 9876543210
+          },
+          { 
+            id: 2,
+            name: 'Nazir',
+            mobile: 9012345678
+          },
+        ]
+      )
+    })
+  }
+}
